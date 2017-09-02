f4da55eb32e87a352fcfe1ab0f171afd0525bbb8
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2017-09-01 22:03:31 -0400

    Building up parse

--- include/parse.h
+++ include/parse.h
@@ -1,41 +1,80 @@
 #ifndef GOLDSCORPION_PARSE
 #define GOLDSCORPION_PARSE
 
-#define GS_CACHED_STRING_MAX 1024
-
 #include <stddef.h>
+#include <setjmp.h>
 #include <stdbool.h>
 #include "lex.h"
 
+struct ASTNode;
+
 typedef enum ASTNodeType {
-  ASTPackageStatement,
-  ASTScopeResolution
+  ASTLiteral,
+  ASTIdentifier,
+  ASTGetter,
+  ASTArgumentList,
+  ASTCall,
+  ASTUnaryExpression
 } ASTNodeType;
 
-typedef struct List_String {
-  char* data;
-  struct List_String* next;
-} List_String;
+typedef struct ExprGet {
+  struct ASTNode* source;
+  char* field;
+} ExprGet;
+
+typedef struct ExprCall {
+  struct ASTNode* source;
+  struct ASTNode* arguments;
+} ExprCall;
+
+typedef struct UnaryExpression {
+  struct ASTNode* rhs;
+  Token op;
+} UnaryExpression;
+
+typedef struct List_Expression {
+  struct ASTNode* data;
+  struct List_Expression* next;
+} List_Expression;
 
 typedef struct ASTNode {
   ASTNodeType type;
   union {
-    List_String* asPackageStatement;
-    List_String* asScopeResolution;
+    // ASTLiteral, ASTIdentifier
+    Token token;
+    // ASTGetter
+    ExprGet get;
+    // ASTArgumentList
+    List_Expression* expressionList;
+    // ASTCall
+    ExprCall call;
+    // ASTUnaryExpression
+    UnaryExpression unaryExpression;
   } data;
 } ASTNode;
 
-typedef struct StatePackage {
+typedef struct Parser {
   List_Token* prev;
   List_Token* current;
-} StatePackage;
-
-typedef struct Parser {
-  StatePackage state;
+  jmp_buf exceptionHandler;
   char* error;
 } Parser;
 
+void gsParserThrow( Parser* self, char* error );
+
 void gsParserIncrement( Parser* self );
+List_Token* gsParserExpect( Parser* self, TokenType type );
+
+ASTNode* gsCreatePrimaryNode( Token token );
+ASTNode* gsCreateGetNode( ASTNode* source, char* field );
+ASTNode* gsCreateCallNode( ASTNode* source, ASTNode* arguments );
+
+ASTNode* gsGetArguments( Parser* self );
+
+ASTNode* gsGetExpressionPrimary( Parser* self );
+ASTNode* gsGetExpressionCall( Parser* self );
+ASTNode* gsGetExpressionUnary( Parser* self );
+ASTNode* gsGetExpression( Parser* self );
 
 Parser* gsGetParser( List_Token* starterToken );
 
--- src/parse.c
+++ src/parse.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#define PRIMARY_TOKENS_COUNT 8
+static const TokenType PRIMARY_TOKENS[ PRIMARY_TOKENS_COUNT ] = { BOOL_TRUE, BOOL_FALSE, NULL_TOKEN, THIS, INTEGER, REAL, STRING, IDENTIFIER };
+
 /*
 List_String* gsGetScopeResolution( Parser* self ) {
   if( self->state.current->data.type != IDENTIFIER ) {
@@ -33,15 +36,188 @@ List_String* gsGetScopeResolution( Parser* self ) {
 }
 */
 
+void gsParserThrow( Parser* self, char* error ) {
+  self->error = error;
+
+  longjmp( self->exceptionHandler, 1 );
+}
+
+/**
+ * If the current token type matches "type", return the token and increment the parser.
+ * Otherwise, return a null pointer.
+ */
+List_Token* gsParserExpect( Parser* self, TokenType type ) {
+  if( !self->current ) {
+    return NULL;
+  }
+
+  if( self->current->data.type == type ) {
+    gsParserIncrement( self );
+    return self->prev;
+  }
+
+  return NULL;
+}
+
+
+ASTNode* gsCreatePrimaryNode( Token token ) {
+  ASTNode* result = calloc( 1, sizeof( ASTNode ) );
+
+  result->type = ( token.type == IDENTIFIER || token.type == THIS ) ? ASTIdentifier : ASTLiteral;
+  result->data.token = token;
+
+  // Deep copy all strings - lexer should be allowed to close itself after an AST is created
+  if( token.type == IDENTIFIER || token.type == STRING ) {
+    result->data.token.literal.asString = calloc( 1 + strlen( token.literal.asString ), sizeof( char ) );
+    strcpy( result->data.token.literal.asString, token.literal.asString );
+  }
+
+  return result;
+}
+
+ASTNode* gsCreateGetNode( ASTNode* source, char* field ) {
+  ASTNode* result = calloc( 1, sizeof( ASTNode ) );
+
+  result->type = ASTGetter;
+  result->data.get.source = source;
+  result->data.get.field = calloc( 1 + strlen( field ), sizeof( char ) );
+  strcpy( result->data.get.field, field );
+
+  return result;
+}
+
+ASTNode* gsCreateCallNode( ASTNode* source, ASTNode* arguments ) {
+  ASTNode* result = calloc( 1, sizeof( ASTNode ) );
+
+  result->type = ASTCall;
+  result->data.call.source = source;
+  result->data.call.arguments = arguments;
+
+  return result;
+}
+
+/**
+ * TODO: if gsGetExpression throws, set local jmp_buf to clean up allocations. Then longjmp to original jmp_buf
+ * Don't give a shit about leaks here for now because when the parser encounters an error, it'll close and the OS will deallocate it.
+ *
+ * Only call this if you have at least one argument (look ahead for the RIGHT_PAREN right after the LEFT_PAREN)
+ */
+ASTNode* gsGetArguments( Parser* self ) {
+  ASTNode* result = calloc( 1, sizeof( ASTNode ) );
+  result->type = ASTArgumentList;
+
+  // Get first expression
+  result->data.expressionList = calloc( 1, sizeof( List_Expression ) );
+  result->data.expressionList->data = gsGetExpression( self );
+
+  List_Expression* prev = result->data.expressionList;
+  while( self->current && self->current->data.type == COMMA ) {
+    // Eat the COMMA
+    gsParserIncrement( self );
+
+    prev->next = calloc( 1, sizeof( List_Expression ) );
+    prev->next->data = gsGetExpression( self );
+    prev = prev->next;
+  }
+
+  return result;
+}
+
+ASTNode* gsGetExpressionPrimary( Parser* self ) {
+  List_Token* match = NULL;
+
+  for( size_t i = 0; i != PRIMARY_TOKENS_COUNT; i++ ) {
+    match = gsParserExpect( self, PRIMARY_TOKENS[ i ] );
+    if( match ) {
+      return gsCreatePrimaryNode( match->data );
+    }
+  }
+
+  if( ( match = gsParserExpect( self, LEFT_PAREN ) ) ) {
+    ASTNode* expression = gsGetExpression( self );
+
+    if( gsParserExpect( self, RIGHT_PAREN ) ) {
+      // return the gsGetExpression result
+      return expression;
+    } else {
+      gsParserThrow( self, "Expected: ) token" );
+    }
+  }
+
+  gsParserThrow( self, "Expected: 'primary' or ( token" );
+
+  // Shuts up lint
+  return NULL;
+}
+
+ASTNode* gsGetExpressionCall( Parser* self ) {
+  ASTNode* expr = gsGetExpressionPrimary( self );
+  List_Token* match = NULL;
+
+  while( self->current && ( self->current->data.type == LEFT_PAREN || self->current->data.type == DOT ) ) {
+    // Don't even do anything else if the left hand side of these exprs are incompatible types
+    if( expr->type == ASTIdentifier || expr->type == ASTCall || expr->type == ASTGetter ) {
+      if( self->current->data.type == LEFT_PAREN ) {
+        // Function call
+        gsParserIncrement( self );
+
+        // If the next token is a RIGHT_PAREN, there's no arguments.
+        if( ( match = gsParserExpect( self, RIGHT_PAREN ) ) ) {
+          expr = gsCreateCallNode( expr, NULL );
+        } else {
+          expr = gsCreateCallNode( expr, gsGetArguments( self ) );
+
+          // Expect a RIGHT_PAREN
+          if( !( match = gsParserExpect( self, RIGHT_PAREN ) ) ) {
+            gsParserThrow( self, "Expected: ) token" );
+          }
+        }
+      } else {
+        // Getter
+        gsParserIncrement( self );
+
+        if( ( match = gsParserExpect( self, IDENTIFIER ) ) ) {
+          expr = gsCreateGetNode( expr, match->data.literal.asString );
+        } else {
+          gsParserThrow( self, "Expected: <identifier> token" );
+        }
+      }
+    } else {
+      gsParserThrow( self, "Invalid left-hand side of operator" );
+    }
+  }
+
+  return expr;
+}
+
+ASTNode* gsGetExpressionUnary( Parser* self ) {
+  if( self->current->data.type == BANG || self->current->data.type == MINUS ) {
+    ASTNode* expr = calloc( 1, sizeof( ASTNode ) );
+    expr->type = ASTUnaryExpression;
+    expr->data.unaryExpression.op = self->current->data;
+    gsParserIncrement( self );
+    expr->data.unaryExpression.rhs = gsGetExpressionUnary( self );
+
+    return expr;
+  } else {
+    return gsGetExpressionCall( self );
+  }
+}
+
+ASTNode* gsGetExpression( Parser* self ) {
+  // TODO !!
+  return NULL;
+}
+
 void gsParserIncrement( Parser* self ) {
-  self->state.prev = self->state.current;
-  self->state.current = self->state.current->next;
+  self->prev = self->current;
+  self->current = self->current->next;
 }
 
 Parser* gsGetParser( List_Token* starterToken ) {
   Parser* parser = calloc( 1, sizeof( Parser ) );
 
-  parser->state.current = starterToken;
+  parser->current = starterToken;
 
   return parser;
 }
