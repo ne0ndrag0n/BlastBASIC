fa770e413d1348fb56a4e82c98cd09cc5c497368
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2021-01-24 19:19:09 -0500

    Compiler loading order

--- include/compiler.hpp
+++ include/compiler.hpp
@@ -9,11 +9,15 @@ namespace GoldScorpion {
 
     struct CompilerSettings {
         std::set< std::string >& activeFiles;
+        std::set< std::string >& resolvedFiles;
         SymbolResolver& symbols;
         bool printLex = false;
         bool printAst = false;
     };
 
+    /**
+     * Return an unverified tree
+     */
     Result< Program, std::string > fileToTree( const std::string& path, CompilerSettings settings );
 
     Result< Program, std::string > fileToProgram( const std::string& path, CompilerSettings settings );
--- src/compiler.cpp
+++ src/compiler.cpp
@@ -11,14 +11,13 @@
 
 namespace GoldScorpion {
 
-	Result< Program, std::string > fileToProgram( const std::string& parseFilename, CompilerSettings settings ) {
+	Result< Program, std::string > fileToTree( const std::string& parseFilename, CompilerSettings settings ) {
 		auto fileResult = Utility::fileToString( parseFilename );
 
 		if( auto file = std::get_if< Utility::File >( &fileResult ) ) {
 			settings.symbols.addFile( parseFilename );
 
 			auto tokenResult = getTokens( file->contents );
-
 			if( auto tokens = std::get_if< std::vector< Token > >( &tokenResult ) ) {
 				printSuccess( "Lexed file " + parseFilename );
 
@@ -28,7 +27,6 @@ namespace GoldScorpion {
 					}
 				}
 
-				// Try this and see if anything crashes
 				auto parserResult = getProgram( *tokens );
 				if( auto program = std::get_if< Program >( &parserResult ) ) {
 					printSuccess( "Parsed file " + parseFilename );
@@ -37,30 +35,66 @@ namespace GoldScorpion {
 						GoldScorpion::printAst( *program );
 					}
 
-					if( auto error = check( parseFilename, *program, settings.symbols ) ) {
-						return Result< Program, std::string >::err( "Failed to validate file " + parseFilename + ": " + *error );
-					} else {
-						printSuccess( "Validated file " + parseFilename );
-						return Result< Program, std::string >::good( std::move( *program ) );
-					}
+					return Result< Program, std::string >::good( std::move( *program ) );
 				} else {
 					return Result< Program, std::string >::err( "Could not parse file " + parseFilename + ": " + std::get< std::string >( std::move( parserResult ) ) );
 				}
-
 			} else {
 				return Result< Program, std::string >::err( "Could not lex file " + parseFilename + ": " + std::get< std::string >( tokenResult ) );
 			}
-
 		} else {
 			return Result< Program, std::string >::err( "Could not open file " + parseFilename + ": " + std::get< std::string >( fileResult ) );
+		}
+	}
+
+	Result< Program, std::string > fileToProgram( const std::string& parseFilename, CompilerSettings settings ) {
+		// Do not do a thing if this file was opened before it was done
+		if( settings.activeFiles.count( parseFilename ) ) {
+			return Result< Program, std::string >::err( "Circular dependency detected: " + parseFilename );
+		}
+
+		// Mark file active while processing
+		// If we try to reload this file before it was processed, it will throw a circular dependency error
+		settings.activeFiles.insert( parseFilename );
+
+		// Lex and parse file
+		Result< Program, std::string > tree = fileToTree( parseFilename, settings );
+
+		// Hand down error if it's an error
+		if( !tree ) {
+			return tree;
+		}
+
+		for( const auto& statement : ( *tree ).statements ) {
+			// Expose file to SymbolResolver so that symbols can be properly exposed
+			if( auto importDeclaration = std::get_if< std::unique_ptr< ImportDeclaration > >( &statement->value ) ) {
+				// Don't reload the file if it was already active
+				if( !settings.resolvedFiles.count( ( *importDeclaration )->path ) ) {
+					auto result = fileToProgram( ( *importDeclaration )->path, settings );
+					if( !result ) {
+						return result;
+					}
+				}
+			}
+		}
+
+		// Validate this file
+		if( auto error = check( parseFilename, *tree, settings.symbols ) ) {
+			return Result< Program, std::string >::err( "Failed to validate file " + parseFilename + ": " + *error );
+		} else {
+			printSuccess( "Validated file " + parseFilename );
+			settings.activeFiles.erase( parseFilename );
+			settings.resolvedFiles.insert( parseFilename );
+			return Result< Program, std::string >::good( std::move( *tree ) );
 		}
 	}
 
     int compile( const std::string& parseFilename, bool printLex, bool printAst ) {
 		SymbolResolver symbols;
 		std::set< std::string > activeFiles;
+		std::set< std::string > resolvedFiles;
 
-		Result< Program, std::string > result = fileToProgram( parseFilename, CompilerSettings{ activeFiles, symbols, printLex, printAst } );
+		Result< Program, std::string > result = fileToProgram( parseFilename, CompilerSettings{ activeFiles, resolvedFiles, symbols, printLex, printAst } );
 
 		if( !result ) {
 			printError( result.getError() );
